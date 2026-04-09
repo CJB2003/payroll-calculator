3d8c2f3f0d34e5f7e286e5866ac6cc32d975940c
chrisbunny <christopherjbunny@gmail.com>
2026-04-09 15:42:21 -0500

    Finished steps #1 and #2

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        //Create new scanner
+        Scanner myScanner = new Scanner(System.in);
+
+        //Ask for name
+        System.out.println("Enter your name: ");
+
+        String userName = myScanner.nextLine();
+
+        //Output employee name
+        System.out.println("Employee name: " + userName);
+
+        //Ask for hours worked
+        System.out.println("How many hours have you worked?");
+
+        double hoursWorked = myScanner.nextDouble();
+
+        //Output hours worked
+        System.out.println("Hours worked: " + hoursWorked);
+
+        //Pay rate is pay rate multiplied by hours worked
+        double payRate = myScanner.nextDouble();
+        double payRatePerHour = payRate * hoursWorked;
+
+        //Output gross pay
+        System.out.println("Pay rate: $" + payRatePerHour);
+
+    }
 }
